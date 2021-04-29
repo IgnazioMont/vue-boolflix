69c0a7a7d61315710ee4384aac5d8d73450bd77a
Ignazio Montebello <ignaziom86@yahoo.it>
2021-04-29 19:45:12 +0200

    Search + 1st overview

--- js/script.js
+++ js/script.js
@@ -1,21 +1,47 @@
-/* 
-	My Netflix
-*/
+//	My Netflix
 
 const boolZapp = new Vue({
 	el: "#root",
 
-	//	Data
+	//	DATA
 	data: {
-	},
-
-	//	Methods
-	methods: {
+		inputUtente:"",
+		/* lingua:"", */
+		showVideo:[],
+		type: "",
+		apiKey:"d5038481a74eaddc438beabb2231c5bd",
+		uri:"https://api.themoviedb.org/3",
+		titleH1: ""
 	},
 
 	mounted() {
+		//	Chiamata iniziale "prossime uscite"
 		axios
-			.get("")
-			.then((response) => { });
+			.get(`${this.uri}/movie/upcoming?api_key=${this.apiKey}`)
+			.then((response) => {
+				this.showVideo = response.data.results;
+				this.type = "movie";
+				this.titleH1 = "Prossime uscite";
+		});
+	},
+
+	//	METHODS
+	methods: {
+		/* Input dell'utente per la ricerca */
+		searchUtente() {
+			if (this.inputUtente != "") {
+				//	Chiamata sia di FILM che di SERIE TV con "/multy"
+				axios
+				//	Richiesta multipla a seconda della input
+					.get(`${this.uri}/search/multi?api_key=${this.apiKey}&query=${this.inputUtente}`)
+					.then((response) => {
+						this.showVideo = response.data.results;
+						this.type = "";
+						this.titleH1 = `Ecco la tua ricerca: ${this.inputUtente}`
+				});
+			}
+		}
+
+		//	Creare una diversificazione del risultato fra film e serieTV!!
 	}
 });
